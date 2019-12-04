b1db2ff63a1af131647ca611d7be2b52e57b3b43
luxo <LeonardoSilva.Zenta@latam.com>
2019-12-04 15:29:13 -0300

    cambios de multipart

--- SpringBootFileUpload/src/main/java/com/roytuts/controller/FileUploadRestController.java
+++ SpringBootFileUpload/src/main/java/com/roytuts/controller/FileUploadRestController.java
@@ -1,22 +1,27 @@
 package com.roytuts.controller;
 import java.io.InputStream;
 import java.util.logging.Logger;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class FileUploadRestController {
     private static final Logger logger = Logger.getLogger(FileUploadRestController.class.getName());
-    @PostMapping("/upload")
-    public ResponseEntity<String> uploadData(@RequestParam("file") MultipartFile file) throws Exception {
+    
+    @RequestMapping(value = "/upload", method = RequestMethod.POST,
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE,MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseEntity<String> uploadData(@RequestPart("file") MultipartFile file, @RequestPart("msg") String msg) throws Exception {
         if (file == null) {
-            throw new RuntimeException("You must select the a file for uploading");
+            throw new RuntimeException("You must select the a file for uploading + msg");
         }
         InputStream inputStream = file.getInputStream();
-        String originalName = file.getOriginalFilename();
+        String originalName = file.getOriginalFilename() +" "+ msg;
         String name = file.getName();
         String contentType = file.getContentType();
         long size = file.getSize();
